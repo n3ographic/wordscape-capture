cc90fd5e0669d4360f052efb107f5879b848a49e
n3ographic <49596395+n3ographic@users.noreply.github.com>
2025-10-21 10:07:27 +0200

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,14 +1,14 @@
-export const setCORS = (res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Max-Age", "600");
-};
-export const okOptions = (req, res) => {
-  if (req.method === "OPTIONS") {
-    setCORS(res);
-    res.status(204).end();
-    return true;
-  }
-  return false;
-};
+// Simple CORS helper (GET/POST/OPTIONS)
+export function withCORS(handler) {
+  return async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Max-Age', '600');
+
+    if (req.method === 'OPTIONS') {
+      return res.status(204).end();
+    }
+    return handler(req, res);
+  };
+}
